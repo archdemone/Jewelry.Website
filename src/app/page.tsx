967415b71f16fc2e115b9026307969ed552c385b
Archdemone <dima.mironovs12@gmail.com>
2025-08-16 13:10:44 +0100

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import HeroSection from '@/components/home/HeroSection'
+import FeaturedProducts from '@/components/home/FeaturedProducts'
+import CategoryShowcase from '@/components/home/CategoryShowcase'
+import TestimonialsSection from '@/components/home/TestimonialsSection'
+import NewsletterSection from '@/components/home/NewsletterSection'
+import WhyChooseUs from '@/components/home/WhyChooseUs'
+import { Header } from '@/components/layout/Header'
+import { Footer } from '@/components/layout/Footer'
+
 export default function Home() {
-	return null
+	return (
+		<>
+			<Header />
+			<main className="container space-y-12 py-10">
+				<HeroSection />
+				<FeaturedProducts />
+				<CategoryShowcase />
+				<WhyChooseUs />
+				<TestimonialsSection />
+				<NewsletterSection />
+			</main>
+			<Footer />
+		</>
+	)
 }
