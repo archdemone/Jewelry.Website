8b7acf6c530bab0096a3833d815fb49ed08e9c5a
Archdemone <dima.mironovs12@gmail.com>
2025-08-16 20:16:44 +0100

    g

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,14 @@
 export const dynamic = 'force-dynamic'
 
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
-	return <main className="min-h-screen bg-white">{children}</main>
+	return (
+		<>
+			{/* Shared header for shop pages */}
+			{require('react').createElement(require('@/components/layout/Header').Header)}
+			<main className="min-h-screen bg-white">{children}</main>
+			{require('react').createElement(require('@/components/layout/Footer').Footer)}
+		</>
+	)
 }
 
 
